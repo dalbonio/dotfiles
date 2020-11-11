dda56fc2fe52571ff87cfd6bbd6147ef65f85fe7
mick_git <mickeylimalopes@gmail.com>
2020-11-10 21:10:31 -0300

    updating gen_dataset.jl: now it can use the command argument to set the dataset rows number

--- zamito/dataset/gen_dataset.jl
+++ zamito/dataset/gen_dataset.jl
@@ -3,10 +3,20 @@ Pkg.activate(".")
 
 using CSV, DataFrames
 
+rows = 119746
+
+if length(ARGS) > 0
+	rows = parse(Int64, ARGS[1])
+end
+
+print("generating a ", rows, " rows dataset...\n")
+
 #matriz aleatoria com numeros de 1 a 10, com 16 linhas, 4 colunas
-R = rand(1:10, 25, 4)
+R = rand(1:10, rows, 4)
 
 #transforma a ultima coluna no valor 0 caso a soma seja menor que 20, e 1 caso seja maior
 R[:, 4] = sum.(eachrow(R[:, 1:3])) .> 20
 
 CSV.write("dataset.csv", DataFrame(R), writeheader=false, delim=" ")
+
+print("dataset generated!\n")
