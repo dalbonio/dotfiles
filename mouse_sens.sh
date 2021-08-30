37d6a641280a9058ce970b9e9cd26c501550ab1f
Lucas Dalbonio <dalbonio2@gmail.com>
2021-08-30 12:46:50 -0300

    adding emacs and changing mouse accel

--- mouse_sens.sh
+++ mouse_sens.sh
@@ -2,4 +2,4 @@
 
 Mouse=`xinput list |grep -i Logitech | awk -F= '{ print $2}'| awk '{print $1}'| awk 'BEGIN{ RS = "" ; FS = "\n" }{print $1}'`
 
-xinput set-prop $Mouse "Coordinate Transformation Matrix" .6 0 0 0 .6 0 0 0 1
+xinput set-prop $Mouse "Coordinate Transformation Matrix" .2 0 0 0 .2 0 0 0 1
