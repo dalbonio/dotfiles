a4a651d79485827a4e4ca99c53b90bcaf9d9e5d1
Lucas Dalbonio <dalbonio2@gmail.com>
2022-11-09 10:14:46 -0300

    update neovim cfg

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,65 +1,216 @@
-require("base")
-require("highlights")
-require("maps")
-require("plugins")
+require('plugins')
 
-local api = vim.api
-vim.g.loaded = 1
-vim.g.loaded_netrwPlugin = 1
-
--- empty setup using defaults
-require("nvim-tree").setup()
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-
-vim.cmd([[
-set encoding=UTF-8
-let g:airline_powerline_fonts = 1
-colorscheme tokyonight-night
-let g:lazygit_floating_window_winblend = 0 " transparency of floating window
-let g:lazygit_floating_window_scaling_factor = 0.9 " scaling factor for floating window
-let g:lazygit_floating_window_corner_chars = ['╭', '╮', '╰', '╯'] " customize lazygit popup window corner characters
-let g:lazygit_floating_window_use_plenary = 0 " use plenary.nvim to manage floating window if available
-let g:lazygit_use_neovim_remote = 1 " fallback to 0 if neovim-remote is not installed
-]])
-
-vim.g.mapleader = ","
--- vim.cmd [[set termguicolors]]
-api.nvim_command [[colorscheme tokyonight-night]]
-
-require('telescope').setup {
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      n = {
-        ['<c-d>'] = require('telescope.actions').delete_buffer
-      }, -- n
-      i = {
-        ["<C-h>"] = "which_key",
-        ['<c-d>'] = require('telescope.actions').delete_buffer
-      } -- i
-    } -- mappings
-  }, -- defaults
-  ...
-} -- telescope setup
-
+vim.g.mapleader = ','
 
 require('nvim_comment').setup()
+
+if(vim.g.vscode == 1) then 
+	vim.cmd([[
+		nnoremap gr <Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>
+		nnoremap <leader>gg <Cmd>call VSCodeNotify('workbench.scm.focus')<CR>
+	]])
+else
+
+	--vim.cmd[[colorscheme tokyonight-night]]
+	vim.cmd[[
+		set termguicolors
+		colorscheme catppuccin
+		autocmd BufNewFile,BufRead *.tsx,*.jsx set filetype=typescriptreact
+		hi tsxTypeBraces guifg=#999999
+		hi tsxTypes guifg=#666666
+		let g:yats_host_keyword = 1
+		" Configuration example
+		let g:floaterm_keymap_new    = '<F7>'
+		let g:floaterm_keymap_prev   = '<F8>'
+		let g:floaterm_keymap_next   = '<F9>'
+		let g:floaterm_keymap_toggle = '<F12>'
+	]]
+	require('nvim-autopairs').setup({
+	  enable_check_bracket_line = false
+	})
+
+---------- LSP Settings
+	local has_any_words_before = function()
+  		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    		return false
+  		end
+  		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	end
+
+	require'lspconfig'.clangd.setup{}
+	require "lsp_signature".setup()
+	vim.o.completeopt = 'menuone,noselect'
+
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+	local cmp = require'cmp'
+	local luasnip = require("luasnip")
+
+	local lspkind = require('lspkind')
+	local source_mapping = {
+	  buffer = "◉ Buffer",
+	  nvim_lsp = "👐 LSP",
+	  nvim_lua = "🌙 Lua",
+	  cmp_tabnine = "💡 Tabnine",
+	  path = "🚧 Path",
+	  luasnip = "🌜 LuaSnip"
+	}
+
+	cmp.setup({
+	  sources = {
+	    { name = 'nvim_lsp' },
+	    { name = 'luasnip' },
+	    { name = 'buffer' },
+	    { name = 'path' },
+	    { name = 'nvim_lua' },
+	  },
+
+	  formatting = {
+	    format = function(entry, vim_item)
+	      vim_item.kind = lspkind.presets.default[vim_item.kind]
+	      local menu = source_mapping[entry.source.name]
+	      if entry.source.name == 'cmp_tabnine' then
+		if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+		  menu = entry.completion_item.data.detail .. ' ' .. menu
+		end
+		vim_item.kind = ''
+	      end
+	      vim_item.menu = menu
+	      return vim_item
+	    end
+	  },
+
+	  snippet = {
+	    expand = function(args)
+	      require('luasnip').lsp_expand(args.body)
+	    end,
+	  },
+	  mapping = {
+
+	    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+	    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+	    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+	    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+	    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+	    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+	    ['<C-Space>'] = cmp.mapping.complete(),
+	    ['<C-e>'] = cmp.mapping.close(),
+	    ['<CR>'] = cmp.mapping.confirm({
+	      behavior = cmp.ConfirmBehavior.Replace,
+	      select = true,
+	    }),
+
+	    ['<Tab>'] = function(fallback)
+	      if cmp.visible() then
+		cmp.select_next_item()
+	      elseif luasnip.expand_or_jumpable() then
+		vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
+	      else
+		fallback()
+	      end
+	    end,
+	    ['<S-Tab>'] = function(fallback)
+	      if cmp.visible() then
+		cmp.select_prev_item()
+	      elseif luasnip.jumpable(-1) then
+		vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
+	      else
+		fallback()
+	      end
+	    end,
+	  },
+	})
+
+	require("luasnip/loaders/from_vscode").load()
+
+-------------------- END LSP SETTINGS
+
+require'lspconfig'.tsserver.setup {}
+require'lspconfig'.tailwindcss.setup {}
+require'lspconfig'.emmet_ls.setup {}
+require'lspconfig'.eslint.setup {}
+
+
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = true,
+    show_current_context_start = true,
+}
+
+require('lualine').setup()
+
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "typescript", "javascript", "css" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+require('formatter').setup({
+  logging = false,
+  filetype = {
+    javascript = {
+        -- prettierd
+       function()
+          return {
+            exe = "prettierd",
+            args = {vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+    },
+    typescript = {
+        -- prettierd
+       function()
+          return {
+            exe = "prettierd",
+            args = {vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+    },
+    typescriptreact = {
+        -- prettierd
+       function()
+          return {
+            exe = "prettierd",
+            args = {vim.api.nvim_buf_get_name(0)},
+            stdin = true
+          }
+        end
+    },
+
+
+    -- other formatters ...
+  }
+})
+
+end
+
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,88 +1,62 @@
-local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
-end
 
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+local has_vscode = vim.g.vscode == 1
+-- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	-- Simple plugins can be specified as strings
+	use 'tpope/vim-surround'
+	use 'terrortylor/nvim-comment'
+	use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim', disable = has_vscode }
+	use 'unblevable/quick-scope'
 
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use {
+	  'nvim-lualine/lualine.nvim',
+	  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+	  disable = has_vscode
+	}
 
-  use 'terrortylor/nvim-comment'
-  use 'tpope/vim-surround'
-  -- plugins go here
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  -- use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
+	use {'kyazdani42/nvim-web-devicons', disable = has_vscode}
+	use { "lukas-reineke/indent-blankline.nvim", disable = has_vscode }
+	use { 'tpope/vim-surround', disable = has_vscode }
+	use { 'windwp/nvim-autopairs', disable = has_vscode }
+	use { 'neovim/nvim-lspconfig' , disable = has_vscode}
+	use { 'hrsh7th/nvim-cmp' , disable = has_vscode}
+	use { 'hrsh7th/cmp-nvim-lsp' , disable = has_vscode}
+	use { 'hrsh7th/cmp-buffer' , disable = has_vscode}
+	use { 'hrsh7th/cmp-path' , disable = has_vscode}
+	use { 'hrsh7th/cmp-cmdline' , disable = has_vscode}
+	use { 'saadparwaiz1/cmp_luasnip' , disable = has_vscode}
+	use { 'L3MON4D3/LuaSnip' , disable = has_vscode}
+	use { 'rafamadriz/friendly-snippets' , disable = has_vscode}
+	use { 'ray-x/lsp_signature.nvim' , disable = has_vscode}
+	-- use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp', disable= has_vscode}
+	use { 'onsails/lspkind-nvim' , disable = has_vscode}
+	use { 'nvim-treesitter/nvim-treesitter' , disable = has_vscode}
+	use { 'folke/tokyonight.nvim' , disable = has_vscode}
+	use { 'Yazeed1s/oh-lucy.nvim' , disable = has_vscode}
 
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'L3MON4D3/LuaSnip'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'folke/zen-mode.nvim'
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
-  -- use 'github/copilot.vim'
+	use { 'herringtondarkholme/yats.vim' , disable = has_vscode}
 
-  use 'tpope/vim-fugitive'
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
-  use {
-    'romgrk/barbar.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' }
-  }
+	use { 'leafgarland/typescript-vim' , disable = has_vscode}
+	use { 'peitalin/vim-jsx-typescript' , disable = has_vscode}
 
+	use { 'RRethy/nvim-base16' , disable = has_vscode}
 
-  -- rust stuff
-  use 'mfussenegger/nvim-dap'
+	use { 'lambdalisue/fern.vim' , disable = has_vscode}
 
-  use 'nvim-tree/nvim-tree.lua'
-  -- use { 'kyazdani42/nvim-web-devicons' }
-  use 'ryanoasis/vim-devicons'
+	use { "catppuccin/nvim", as = "catppuccin", disable = has_vscode }
 
-  -- colorscheme
-  use 'folke/tokyonight.nvim'
+	use { 'ibhagwan/fzf-lua',
+	  -- optional for icon support
+	  requires = { 'kyazdani42/nvim-web-devicons' }
+	}
 
-  -- help to visualize hotkeys
-  use 'liuchengxu/vim-which-key'
+	use { 'voldikss/vim-floaterm' , disable = has_vscode}
 
-  -- tmux
-  use({
-    "aserowy/tmux.nvim",
-    config = function() require("tmux").setup() end
-  })
+	use { 'mhartington/formatter.nvim', disable = has_vscode }
 
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-
-  use 'kdheepak/lazygit.nvim'
-
-  use {
-  "folke/trouble.nvim",
-  requires = "kyazdani42/nvim-web-devicons",
-  config = function()
-    require("trouble").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
-}
 end)
+
